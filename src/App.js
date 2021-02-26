4d4ce4cc63d4b56c6fe2949a467ed689de77f9b1
DCartist <pbannerman13@gmail.com>
2021-02-26 13:47:04 -0500

    updating app

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         {/* <Route path="/intermediate" exact component={Intermediate}/> */}
         {/* <Route path="/contact" exact component={Contact}/> */}
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
